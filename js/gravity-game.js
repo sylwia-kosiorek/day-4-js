afea61230fce9c38e02ef448efbba2213781d682
Sylwia Kosiorek <sylwiaxkosiorek@gmail.com>
2019-06-27 10:35:51 +0200

    gravity game without moving

--- js/gravity-game.js
+++ js/gravity-game.js
@@ -6,3 +6,24 @@ const earthGravityAcceleration = -9.81
 
 let position = 0
 let velocity = 0
+
+function applyStylesToAlienAndPlanet() {
+    alienElement.style.width = '50px'
+    alienElement.style.height = '50px'
+    alienElement.style.backgroundColor = 'green'
+
+    alienElement.style.position = 'absolute'
+    alienElement.style.left = '50%'
+    alienElement.style.bottom = position + 'px'
+    alienElement.style.transform = 'translateX(-50%)'
+
+    planetElement.style.width='100%'
+    planetElement.style.height='300px'
+    planetElement.style.posiotion= 'relative'
+    planetElement.style.backgroundColor = 'grey'
+
+    planetElement.appendChild(alienElement)
+    containerElement.appendChild(planetElement)
+}
+
+applyStylesToAlienAndPlanet()
