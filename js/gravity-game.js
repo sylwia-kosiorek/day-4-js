3cdd32707d36e63cb3fa679676d0b2acaf0dbe14
Sylwia Kosiorek <sylwiaxkosiorek@gmail.com>
2019-06-27 10:29:21 +0200

    Gravity game started

---
+++ js/gravity-game.js
@@ -0,0 +1,8 @@
+const containerElement = document.querySelector('.gravity-game')
+const alienElement = document.createElement('div')
+const planetElement = document.createElement('div')
+
+const earthGravityAcceleration = -9.81
+
+let position = 0
+let velocity = 0
