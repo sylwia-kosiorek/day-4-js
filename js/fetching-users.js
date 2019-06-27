dea1b95db2b5a501a9f6fdb9a509986661ff58bb
Sylwia Kosiorek <sylwiaxkosiorek@gmail.com>
2019-06-27 14:45:47 +0200

    Further changes, FOR used

--- js/fetching-users.js
+++ js/fetching-users.js
@@ -1,10 +1,22 @@
-(function() {
+(function () {
     const mainContainerElement = document.querySelector('.fetching-users')
 
-    function renderListItem (name, img) {
-        const containerElement = document.createElement ('div')
-        const imgElement = document.createElement ('img')
-        const nameElement = document.createElement ('p')
+    let users = [
+        {
+            name: {
+                first: 'Mateusz',
+                last: 'Choma'
+            },
+            picture: {
+                thumbnail: 'https://randomuser.me/api/portraits/women/52.jpg'
+            }
+        }
+    ]
+
+    function renderListItem(name, img) {
+        const containerElement = document.createElement('div')
+        const imgElement = document.createElement('img')
+        const nameElement = document.createElement('p')
 
         containerElement.style.display = 'flex'
         imgElement.style.borderRadius = '50%'
@@ -22,10 +34,20 @@
 
     }
 
-    function render () {
-        const item = renderListItem ('Mateusz Choma', 'https://randomuser.me/api/portraits/women/52.jpg')
+    function render() {
 
-        mainContainerElement.appendChild(item)
+        for (let i = 0; i < users.length; i++) {
+            const user = users[i]
+
+            const firstName = user.name.first
+            const lastName = user.name.last
+            const img = user.picture.thumbnail
+            const name = firstName + ' ' + lastName
+
+            const item = renderListItem(name, img)
+
+            mainContainerElement.appendChild(item)
+        }
     }
 
     render()
